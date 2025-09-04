f2e3ac5b027827dee6aa4121841f0589a5845c0d
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2025-09-04 17:57:25 +0000

    basic setup for arithmetic calculation

--- src/main.c
+++ src/main.c
@@ -1,18 +1,36 @@
 #include "include/common.h"
 #include "include/chunk.h"
 #include "include/debug.h"
+#include "include/vm.h"
 
 int main(void)
 {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN, 1);
-
+    
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 2);
     writeChunk(&chunk, (uint8_t)constant, 2);
+    
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 3);
+    writeChunk(&chunk, (uint8_t)constant, 3);
+    
+    writeChunk(&chunk, OP_ADD, 3);
+    
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 3);
+    writeChunk(&chunk, (uint8_t)constant, 3);
+    
+    writeChunk(&chunk, OP_DIVIDE, 4);
+    writeChunk(&chunk, OP_NEGATE, 4);
 
-    disassembleChunk(&chunk, "test chunk");
+    writeChunk(&chunk, OP_RETURN, 1);
+
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
